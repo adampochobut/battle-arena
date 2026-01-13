9c7e5629b4adf09c3ecf1b6e238d2475bc71492e
vkini19 <varnkini@gmail.com>
2026-01-13 14:19:24 +0300

    Added basic arena scene with a red and blue team spawn point, and walls

---
+++ Assets/Scipts/ArenaManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("ArenaManager running");
+    }
+
+    void Update()
+    {
+        
+    }
+}
